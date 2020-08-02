2a5ae94e40f5a677eba716cd7735eaeef5203db5
Davis Kauffmann <daviskauffmann@gmail.com>
2020-08-02 12:11:04 -0400

    Temperature type

--- src/models/Temperature.ts
+++ src/models/Temperature.ts
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
+export interface Temperature extends mongoose.Document {
+    value: number;
+    date: Date;
+}
+
 const schema = new mongoose.Schema({
     value: Number,
     date: Date,
 });
 
-const Temperature = mongoose.model('Temperature', schema);
+const Temperature = mongoose.model<Temperature>('Temperature', schema);
 
 export default Temperature;
