9ebf86557eb2848cf7682e5eff58a34a3f927fba
Davis Kauffmann <daviskauffmann@gmail.com>
2021-08-11 14:19:24 -0400

    Remove unused user entity decorators

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,44 +1,19 @@
-import { IsNumber, IsString } from 'class-validator';
-import { Field, ObjectType } from 'type-graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'user' })
-@ObjectType({
-    description: 'User',
-})
 export default class User {
     @PrimaryGeneratedColumn()
-    @Field({
-        description: 'ID',
-    })
-    @IsNumber()
     id!: number;
 
     @Column({ unique: true })
-    @Field({
-        description: 'Username',
-    })
-    @IsString()
     username!: string;
 
     @Column()
-    @Field({
-        description: 'Password',
-    })
-    @IsString()
     password!: string;
 
     @Column({ unique: true })
-    @Field({
-        description: 'Email',
-    })
-    @IsString()
     email!: string;
 
     @Column({ type: 'text', array: true })
-    @Field(() => [String], {
-        description: 'Roles',
-    })
-    @IsString({ each: true })
     roles!: string[];
 }
